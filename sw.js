46afc2f3e1002caaad63d71c387d91cc28a7ea1b
dony102 <donyespapa@gmail.com>
2026-01-08 00:51:10 +0700

    Remove Template feature to match Extension

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Enables offline caching and PWA installation
  */
 
-const CACHE_NAME = 'promptforge-mobile-v16';
+const CACHE_NAME = 'promptforge-mobile-v17';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
