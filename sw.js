488085bb751976637a67cc0479caf1a64a84925e
dony102 <donyespapa@gmail.com>
2026-01-07 23:16:30 +0700

    Force cache v3

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Enables offline caching and PWA installation
  */
 
-const CACHE_NAME = 'promptforge-mobile-v2';
+const CACHE_NAME = 'promptforge-mobile-v3';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
