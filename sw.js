cdade91f111cafcbd9b875f27c30a84931b7394b
dony102 <donyespapa@gmail.com>
2026-01-08 00:26:38 +0700

    Complete UI upgrade: Copy Extension CSS with mobile overrides

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Enables offline caching and PWA installation
  */
 
-const CACHE_NAME = 'promptforge-mobile-v13';
+const CACHE_NAME = 'promptforge-mobile-v14';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
