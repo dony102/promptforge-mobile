f489ba6c8624a82d668d584d56ed4feabaf58cf3
dony102 <donyespapa@gmail.com>
2026-01-08 10:44:55 +0700

    Update PWA UI: remove Windows shortcuts, add mobile instructions

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Enables offline caching and PWA installation
  */
 
-const CACHE_NAME = 'promptforge-mobile-v25';
+const CACHE_NAME = 'promptforge-mobile-v26';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
